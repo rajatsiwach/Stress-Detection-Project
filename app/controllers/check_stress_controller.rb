6f4b054dedf201b617547d7b2f679671b81890d9
Rajat Siwach <rajat24@hotmail.co.in>
2020-12-24 10:37:30 +0530

    deploy final

--- app/controllers/check_stress_controller.rb
+++ app/controllers/check_stress_controller.rb
@@ -1,21 +1,6 @@
 class CheckStressController < ApplicationController
 
-  def index
-    @information = Information.find(1)
-  end
-
   def new
-    @information = Information.new
   end
-
-  def create
-    @sinformation = params[:check_stress][:information]
-    @information = Information.new(working_Hours: params[:check_stress][:information])
-    @information.save
-    redirect_to info_url
-  end
-
-  # def show
-  #   @information = User.find_by(id: params[:id])
-  # end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :check_stress
   root 'check_stress#new'
-  get '/check' , to: 'check_stress#new'
-  post '/check', to: 'check_stress#create'
+  get 'check' , to: 'check_stress#new'
+  post 'check', to: 'check_stress#create'
   get 'phyisological'  , to: 'static_pages#phyisological'
   get 'mental' , to: 'static_pages#mental'
   
